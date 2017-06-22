e15f62fd06e9e91d61aad647b557eaa3f7e242b2
DaViD <DaViD@david>
2017-06-22 04:57:40 -0500

    sodoku

---
+++ src/sudoku/Aplication.java
@@ -0,0 +1,14 @@
+
+package sudoku;
+public class Aplication {
+
+    public static void main(String[] args) {
+       Interface interfaz=new Interface();
+        interfaz.setVisible(true);
+        //interfaz.colocar();
+        interfaz.colocar1();
+        interfaz.llenarIntro();
+        interfaz.imprimirCronometro();
+    }
+    
+}
